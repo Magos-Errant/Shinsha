669ebc3596bdfb058c77647352c28eacb10f9d9e
Magos-Errant <qbit666@gmail.com>
2021-02-09 00:16:23 +0900

    updated gitignore

--- test.py
+++ test.py
@@ -1,18 +1,6 @@
-import NyaaPy
+message = 'boku_no_pico'
+zło = ['boku', 'no', 'pico']
 
-Arr = NyaaPy.Nyaa
-
-x = Arr.search('tatoeba last dungeon 05')
-print(x)
-
-_selected_animu = []
-for _dict in x:
-    if _dict['category'] == 'Anime - English-translated':
-        _selected_animu.append(_dict)
-    else:
-        continue
-
-i = 0
-for _dicts in _selected_animu:
-    print(f"Name: {x[i]['name']}\nYer booty isle: {x[i]['url']}\nCoffer!: {x[i]['download_url']}\nMayteys: {x[i]['seeders']}\nScallywags: {x[i]['leechers']}\nHeave Ho: {x[i]['size']}\n\n")
-    i += 1
+for element_zła in zło:
+    if element_zła in message:
+        print('Zło znaleziono')
