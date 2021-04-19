99d1bfce15468decdeff669a10914b6f4c5bb5bb
Magos-Errant <qbit666@gmail.com>
2021-04-20 00:41:10 +0900

    User mention functionality improvements

--- utils/bot_functions.py
+++ utils/bot_functions.py
@@ -338,12 +338,15 @@ class ShinshaBrain(discord.Client):
     async def CustomMentionsRegister(self, message):
         keywords = message.content.split()[1:]
         user_id = message.author.id
+        _user_name = message.author.mention
 
         if user_id not in data_container.UserCustomMentions:
             data_container.UserCustomMentions[user_id] = []
         for word in keywords:
             if word not in data_container.UserCustomMentions[user_id]:
                 data_container.UserCustomMentions[user_id].append(word)
+
+        await message.channel.send(f'Dodano następujące wzmianki {keywords} dla {_user_name}')
         keywords.clear()
 
     async def CustomMentionsDelete(self, message):
@@ -357,11 +360,14 @@ class ShinshaBrain(discord.Client):
             if word in data_container.UserCustomMentions[_user_id]:
                 data_container.UserCustomMentions[_user_id].remove(word)
         await message.channel.send(
-            f'Zapisane higlighty dla użytkownika {_user_name}:\n{data_container.UserCustomMentions[_user_id]}')
+            f'Zapisane wzmianki dla użytkownika {_user_name}:\n{data_container.UserCustomMentions[_user_id]}')
 
     async def CustomMentionsCheck(self, message):
         _user_name = message.author.mention
         _user_id = message.author.id
+        if _user_id not in data_container.UserCustomMentions:
+            await message.channel.send(f'Brak zarejestrowanych wzmianek {_user_name}')
+            return
         await message.channel.send(f'Zapisane wzmianki dla użytkownika {_user_name}:\n{data_container.UserCustomMentions[_user_id]}')
 
     async def CheckForMentions(self, message):
