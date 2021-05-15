2b63c604026b7c959edeb6cb7b9ed957aaabe47e
Magos-Errant <qbit666@gmail.com>
2021-05-15 21:16:11 +0900

    Bot will now not post humorous responses on given channel

--- utils/HumorousRandomResponsesFunctionality.py
+++ utils/HumorousRandomResponsesFunctionality.py
@@ -1,5 +1,6 @@
 import random
 from .Parameters import *
+from .IDnames import yagoo_channel
 
 async def HumourRegister(message):
     sentence = message.content.split(' ', 1)[1]
@@ -13,5 +14,7 @@ async def HumourRegister(message):
     data_container.store_data()
 
 async def RandomHumour(message):
-    if random.randint(1,100) <= humour_probability:
+    if message.channel.id == yagoo_channel:
+        return
+    elif random.randint(1,100) <= humour_probability:
         await message.channel.send(data_container.humour[random.randint(0,len(data_container.humour))])
