ac6dff2d5e6cf2baeefdff954e8e244ac6037d68
Magos-Errant <qbit666@gmail.com>
2022-01-10 23:06:03 +0900

    Change backtrack
    Problem with library

--- utils/DanbooruFunctionality.py
+++ utils/DanbooruFunctionality.py
@@ -3,23 +3,18 @@ from pybooru import Danbooru
 from .Parameters import *
 from .IDnames import *
 
+from urllib.request import urlopen
+import json
 
-# Main functions
+
+#Main functions
 
 async def danbo(message):
     _tags = rating_formatter(message)
     if message.channel.id == bot_channel or message.guild:
-        _i = 10
-        image_sent = False
-        answer = ''
-        while _i != 0 or image_sent == True:
-            try:
-                picture = picture_generator(_tags)
-                picture = next(picture)
-                answer = await waiting_and_responding(_tags, data_container.banned_tags, message, picture)
-                image_sent = True
-            except Exception:
-                _i -= 1
+        picture = picture_generator(_tags)
+        picture = next(picture)
+        answer = await waiting_and_responding(_tags, data_container.banned_tags, message, picture)
         await message.channel.send(answer)
     else:
         await message.delete()
@@ -49,12 +44,15 @@ async def danbo_count(message):
         else:
             await message.channel.send(f'{this_many_posts} posts found for tags {_tags}, I will leave it here :3')
 
-
-# Functions used by main functions
+#Functions used by main functions
 def picture_generator(_tags):
+    picture = ''
     danbo_client = Danbooru('danbooru')
     while 1:
-        picture = danbo_client.post_list(tags=_tags, limit=1, random=True)
+        try:
+            picture = danbo_client.post_list(tags=_tags, limit=1, random=True)
+        except Exception as e:
+            print(e)
         yield picture
 
 
